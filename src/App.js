c37b1d4c99afbb8138e274135c5c1b486e0c59b2
Martin Giura <martin.giura@gmail.com>
2021-12-08 17:29:07 -0600

    Proyecto de repaso reactusestate terminado

--- src/App.js
+++ src/App.js
@@ -45,8 +45,44 @@ function App() {
       <button onClick={() => generarMensaje()}> Crear mensaje</button>
       <hr />
       <h2> Crea tu comentario</h2>
+
+      <form
+        onSubmit={(event) => {
+          agregarComentario(event);
+        }}
+      >
+        <h3>Escribe un titulo</h3>
+        <input
+          name="title"
+          value={comentario.title}
+          onChange={(event) => {
+            capturarDatos(event);
+          }}
+        />
+        <h3>Escribe una descripcion</h3>
+        <input
+          name="description"
+          value={comentario.description}
+          onChange={(event) => capturarDatos(event)}
+        />
+        <br />
+        <br />
+        <button>Crear comentario</button>
+      </form>
+
+      <hr />
+
+      <h2>Listado de Comentarios</h2>
+
+      {listadoComentarios.map((elem) => {
+        return (
+          <>
+            <h4>{elem.title}</h4>
+            <p>{elem.description}</p>
+          </>
+        );
+      })}
     </>
   );
 }
-
 export default App;
